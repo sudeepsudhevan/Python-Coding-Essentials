3b9ff3f983934a84e31753be5ec2b006446c2319
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2024-03-11 18:18:00 +0530

    Create largest_of_3anothermethod.py

---
+++ Python Assignment/largest_of_3anothermethod.py
@@ -0,0 +1,13 @@
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+num3 = int(input('Enter the third number: '))
+
+
+if num1 > num2:
+    if num1 > num3:
+        print(f"{num1} is the largest number")
+    else:
+        if num2 < num3:
+            print(f"{num3} is the largest number")
+else:
+    print(f"{num2} is the largest number")
